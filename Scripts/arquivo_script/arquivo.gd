09903f72dc9e4b529a5e5ba54a989484f72984e6
SrDoppelganger <arthur.albuquerque2004@gmail.com>
2026-02-18 16:29:44 -0300

    working on basic game loop

---
+++ Scripts/arquivo_script/arquivo.gd
@@ -0,0 +1,13 @@
+extends Node2D
+
+func _on_area_2d_mouse_entered() -> void:
+	print("MOUSE!");
+
+func _on_area_2d_mouse_exited() -> void:
+	print("bye bye");
+
+func _on_area_2d_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+	if event is InputEventMouseButton:
+		if event.pressed:
+			print("bweh bweh");
+			queue_free();
