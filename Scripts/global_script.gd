36319722f3b443c5a52c009abac397c214ac063c
SrDoppelganger <arthur.albuquerque2004@gmail.com>
2026-02-18 17:20:19 -0300

    added basic score system

---
+++ Scripts/global_script.gd
@@ -0,0 +1,7 @@
+extends Node2D
+
+var score: int = 0;
+
+# Caso necessário, add param. de valor para pontuações diferentes
+func addScore():
+	score += 1;
---
+++ Scripts/main_script.gd
@@ -0,0 +1,12 @@
+extends Node2D
+
+@onready var score: Label = $HUD/Score
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	score.text = "Pontos: " + str(GlobalScript.score);
