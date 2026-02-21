a9920def3d209668bd41096066de56c860a8526a
SrDoppelganger <arthur.albuquerque2004@gmail.com>
2026-02-20 23:12:58 -0300

    still working in stamp minigame

--- Scripts/arquivo_script/arquivo.gd
+++ Scripts/arquivo_script/arquivo.gd
@@ -1,13 +1,15 @@
 extends Node2D
 @onready var area_2d: Area2D = $Area2D
 
-const SPEED = 0.7;
+const SPEED = 1;
 var velocity: Vector2;
 var target_position: Vector2;
+var spawn_position: Vector2;
 
 func _ready() -> void:
-	var target_x = randf_range(20, 1122);
-	var target_y = randf_range(150, 625);
+	var target_x = randi_range(200, 1000);
+	var target_y = randi_range(150, 625);
+	spawn_position = position;
 	
 	target_position = Vector2(target_x, target_y);
 	
@@ -17,10 +19,10 @@ func _process(delta: float) -> void:
 	
 
 func _on_area_2d_mouse_entered() -> void:
-	print("MOUSE!");
+	pass;
 
 func _on_area_2d_mouse_exited() -> void:
-	print("bye bye");
+	pass;
 
 func _on_area_2d_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
 	if event is InputEventMouseButton:
@@ -28,3 +30,10 @@ func _on_area_2d_input_event(viewport: Node, event: InputEvent, shape_idx: int) 
 			GlobalScript.playEffect("stamp");
 			GlobalScript.addScore();
 			queue_free();
+
+# Faz o arquivo sair da mesa
+func _on_timer_timeout() -> void:
+	var x = randi_range(28,1143);
+	var y = randi_range(370, 400);
+	
+	target_position = Vector2();
