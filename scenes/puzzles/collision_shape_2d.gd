36c2fc2f9a81955ddc84cd2a9c297c438500ebd5
Lucas <lucashenrique4848@gmail.com>
2026-02-21 02:28:32 -0300

    colisão e escala

--- scenes/puzzles/collision_shape_2d.gd
+++ scenes/puzzles/collision_shape_2d.gd
@@ -1,5 +1,8 @@
 extends CollisionShape2D
 
+func _ready() -> void:
+	position.x = randf_range(-250, 750)
+	position.y = randf_range(-43, 648)
 
 func _on_area_2d_area_entered(area: Area2D) -> void:
 	print("colidiu")
