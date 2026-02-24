a480ece77170577a9a9086f242a86223727575ae
SrDoppelganger <arthur.albuquerque2004@gmail.com>
2026-02-24 18:26:57 -0300

    updated dialogues

--- SceneHandler.gd
+++ SceneHandler.gd
@@ -1,10 +1,35 @@
 #Script para que ajudar a tocar os dialogos certos na hora certa.
 extends Node
 
-var scene = 'intro';
 
-func changeScenario(scenario:String):
-	scene = scenario;
+#para saber qual minigame já foi feito
+var intro:bool = true;
+var minigame1:bool = false;
+var minigame2:bool = false;
+var minigame3:bool = false;
+
+func clearMinigame(scenario:String):
+	match scenario:
+		"minigame1":
+			intro = false;
+			minigame1 = true;
+		"minigame2":
+			intro = false;
+			minigame1 = false
+			minigame2 = true;
+		"minigame3":
+			intro = false;
+			minigame1 = false;
+			minigame2 = false;
+			minigame3 = true;
 	
-func getScenario():
-	return scene;
+func getLastMinigame(minigame:String):
+	match minigame:
+		"intro":
+			return intro;
+		"minigame1":
+			return minigame1;
+		"minigame2":
+			return minigame2;
+		"minigame3":
+			return minigame3;
