50be54bcb2bff71e5a0208b7aab4ccf86804b3fc
Lucas <lucashenrique4848@gmail.com>
2026-02-26 09:34:41 -0300

    captcha base

--- scenes/puzzles/captcha_script.gd
+++ scenes/puzzles/captcha_script.gd
@@ -2,6 +2,10 @@ extends Node2D
 
 @onready var line_edit: LineEdit = $CanvasLayer/LineEdit
 @onready var label: Label = $CanvasLayer/Label
+@onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
+
+var attempt: int = 0
+var text: String = ""
 
 func _ready():
 	#detecta texto que está dentro da caixa de texto
@@ -9,4 +13,22 @@ func _ready():
 
 #função que pega o texto que o usuário digitou
 func _on_lineEdit_text_entered(input_text: String):
-	label.text = input_text;
+	# n ter ++ me deixa triste
+	attempt += 1
+
+	if attempt < 3:
+		fail_captcha()
+	else:
+		success_captcha()
+		
+
+func fail_captcha():
+	await get_tree().create_timer(1.0).timeout
+	label.text = "Incorreto, tente novamente"
+	
+	line_edit.clear()
+	
+func success_captcha():
+	label.text = "Verificado!"
+	
+	line_edit.editable = false
