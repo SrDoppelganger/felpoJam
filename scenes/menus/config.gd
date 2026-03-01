647427ee3cee565b908a60ae076b4ed3e1286ef5
SrDoppelganger <arthur.albuquerque2004@gmail.com>
2026-03-01 02:32:33 -0300

    waiting for the final assets to be done

--- scenes/menus/config.gd
+++ scenes/menus/config.gd
@@ -11,8 +11,6 @@ func _on_volume_master_value_changed(value: float) -> void:
 	else:
 		AudioServer.set_bus_mute(0,false);
 		AudioServer.set_bus_volume_db(0,value);
-func _on_mute_master_toggled(toggled_on: bool) -> void:
-	AudioServer.set_bus_mute(0,toggled_on);
 
 #VOLUME MÚSICA
 func _on_volume_musica_value_changed(value: float) -> void:
@@ -22,9 +20,6 @@ func _on_volume_musica_value_changed(value: float) -> void:
 		AudioServer.set_bus_mute(1,false);
 		AudioServer.set_bus_volume_db(1,value);
 
-func _on_mute_musica_toggled(toggled_on: bool) -> void:
-	AudioServer.set_bus_mute(1,toggled_on);
-
 #VOLUME EFEITOS SONOROS
 func _on_volume_efeito_value_changed(value: float) -> void:
 	if value == -50.0:
@@ -32,8 +27,6 @@ func _on_volume_efeito_value_changed(value: float) -> void:
 	else:
 		AudioServer.set_bus_mute(2,false);
 		AudioServer.set_bus_volume_db(2,value);
-func _on_mute_efeito_toggled(toggled_on: bool) -> void:
-	AudioServer.set_bus_mute(2,toggled_on);
 
 
 func _on_button_pressed() -> void:
